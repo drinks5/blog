16576c69a1096f14f2a582d0053e02ebf7436def
drinksober <drinksober@foxmail.com>
2015-10-13 19:02:00 +0800

    fix the comment bug

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.views.static import serve
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/doc/',include('django.contrib.admindocs.urls')),
     url(r'^accounts/', include('accounts.urls')),
@@ -12,4 +12,4 @@ urlpatterns = patterns('',
     url(r'^(?P<page>\d*)$', 'article.views.home', name='home'),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT }),
     url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
-    )
+    ]
