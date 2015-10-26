4869fa466c4a209f7b8e6b97bdeb47635190823b
drinksober <drinksober@foxmail.com>
2015-10-26 14:04:06 +0800

    add test and rm the apache, which instead by nginx

--- article/tests.py
+++ article/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
+
+from accounts.models import User
+from article.models import Article, Comment, Sort
 #Create your tests here.
 
 
+user = User.objects.all()[0]
 
 class ArticleMethodTest(TestCase):
-
-    pass
+    def craete(self):
+        Article.objecs.create(title='test',author=user, sort=Sort.objects.get(id=1))
