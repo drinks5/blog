b9df690076ca4c2bc30a60d1b4d9691298a9855f
drinks <drinks.huang@hypers.com>
2016-03-09 13:59:10 +0800

    .

--- manage.py
+++ manage.py
@@ -4,8 +4,6 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
-    # os.environ["DJANGO_SETTINGS_MODULE"] = "conf.settings"
-    # print(os.environ.get('DJANGO_SETTINGS_MODULE'))
 
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
