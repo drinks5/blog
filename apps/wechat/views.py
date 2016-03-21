3824aaeb7792eed53465acc6c1c342216fb43a24
drinks <drinksober@foxmail.com>
2016-03-21 22:44:00 +0800

    debug

--- apps/wechat/views.py
+++ apps/wechat/views.py
@@ -2,7 +2,7 @@
 # @Author: drinks
 # @Date:   2016-03-21 21:17:50
 # @Last Modified by:   drinks
-# @Last Modified time: 2016-03-21 21:44:34
+# @Last Modified time: 2016-03-21 22:42:40
 
 import hashlib
 
@@ -23,13 +23,13 @@ def tokenView(request):
         tmp_list.sort()
         tmp_str = "%s%s%s" % tuple(tmp_list)
         tmp_str = hashlib.sha1(tmp_str).hexdigest()
-        if tmp_str == signature:
+        if check_signature(signature, timestamp, nonce, token):
             return HttpResponse(echostr)
         else:
             return HttpResponse("weixin  index")
 
-def check_signature(signature, timestamp, nonce):
-    L = [timestamp, nonce, token]
+def check_signature(signature, timestamp, nonce, token):
+    L = [timestamp, nonce, TOKEN]
     L.sort()
     s = L[0] + L[1] + L[2]
     return hashlib.sha1(s).hexdigest() == signature
